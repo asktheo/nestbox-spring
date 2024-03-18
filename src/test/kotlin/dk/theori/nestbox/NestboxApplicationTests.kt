0ee58fecf2be821b0569a5c6ff4cb5d2ad413294
Theo Askov <christianelbek@gmail.com>
2024-03-19 00:25:29 +0100

    First commit of empty Spring application in Kotlin.

---
+++ src/test/kotlin/dk/theori/nestbox/NestboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package dk.theori.nestbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NestboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
