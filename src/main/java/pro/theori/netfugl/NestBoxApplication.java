392b848654a9d656d6f1472c6cae222f7759084e
Theo Askov <christianelbek@gmail.com>
2024-03-24 17:07:54 +0100

    1 - backend: Spring framework, repository and datamodel
    Ignored files
    First commit of empty Spring application in Java
    Ddatamodel for nestbox and nestboxrecord

---
+++ src/main/java/pro/theori/netfugl/NestBoxApplication.java
@@ -0,0 +1,12 @@
+package pro.theori.netfugl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestBoxApplication.class, args);
+	}
+}
