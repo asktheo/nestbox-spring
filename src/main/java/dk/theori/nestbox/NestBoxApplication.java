4b6e993f3eb28cd23e92b0f19acd5fc07e32b83a
Theo Askov <christianelbek@gmail.com>
2024-03-25 01:50:50 +0100

    1 - backend: Working application
    Introduced env. variable JSON_DIR for reading data from a file into the database
    Package as war
    Changed mongo port to default for deployment ease

--- src/main/java/dk/theori/nestbox/NestBoxApplication.java
+++ src/main/java/dk/theori/nestbox/NestBoxApplication.java
@@ -2,7 +2,6 @@ package dk.theori.nestbox;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.tools.javac.Main;
 import dk.theori.nestbox.entities.NestBox;
 import dk.theori.nestbox.entities.NestBoxFeatureCollection;
 import dk.theori.nestbox.repositories.NestBoxMongoRepository;
@@ -11,9 +10,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
 
 @SpringBootApplication
@@ -22,7 +20,7 @@ public class NestBoxApplication implements CommandLineRunner {
 	@Autowired
 	private NestBoxMongoRepository nestBoxMongoRepository;
 
-	private boolean isProduction = true;
+	private static final String JSON_DIR = System.getenv("JSON_DIR");
 
 	public static void main(String[] args) {
 		SpringApplication.run(NestBoxApplication.class, args);
@@ -31,15 +29,14 @@ public class NestBoxApplication implements CommandLineRunner {
 
 	public void run(String... args){
 
-		if(nestBoxMongoRepository.findAll().size()==0){
+		if(nestBoxMongoRepository.findAll().isEmpty()){
 			try{
 				Integer count = insertNestBoxesFromFile();
 				System.out.printf("%s nest boxes inserted in Mongo DB", count);
 			}
 			catch(Exception e){
-				System.out.println(e);
+				System.out.println(e.getMessage());
 			}
-			System.out.println();
 		}
 	}
 
@@ -47,7 +44,10 @@ public class NestBoxApplication implements CommandLineRunner {
 		ObjectMapper objectMapper = new ObjectMapper();
 
 		// Read JSON resource file
-		InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("nestboxes.json");
+		File nestboxFile = new File(JSON_DIR + "/nestboxes.json");
+		FileInputStream inputStream = new FileInputStream(nestboxFile);
+
+		//InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("nestboxes.json");
 		NestBoxFeatureCollection  featureCollection= objectMapper.readValue(inputStream, new TypeReference<>() {
 		});
 		List<NestBox> nestboxes = nestBoxMongoRepository.insert(featureCollection.getFeatures());
